a3ca14eca438460501bc78ff88d903285afb9f03
RonilRufo <ronil.rufo@gmail.com>
2018-03-05 21:56:59 +0800

    Added PizzaVariation model

--- product/models.py
+++ product/models.py
@@ -22,9 +22,7 @@ class PizzaSize(models.Model):
 
 class Pizza(models.Model):
     """
-    Stores information about a Pizza. Also stores
-    a list of :model:`product.PizzaSize` available
-    to a certain pizza.
+    Stores basic information about a Pizza.
     """
     name = models.CharField(
         max_length=64,
@@ -37,3 +35,31 @@ class Pizza(models.Model):
 
     def __str__(self):
         return "{}".format(self.name)
+
+
+class PizzaVariation(models.Model):
+    """
+    Stores available variations for each
+    :model:`product.Pizza`. Variations are represented
+    by :model:`product.PizzaSize`
+    """
+    pizza = models.ForeignKey(
+        'product.Pizza',
+        related_name='variations',
+        on_delete=models.CASCADE,
+    )
+    size = models.ForeignKey(
+        'product.PizzaSize',
+        related_name='pizza_variations',
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    price = models.DecimalField(
+        max_digits=7,
+        decimal_places=2,
+    )
+
+    class Meta:
+        verbose_name = _('Pizza Variation')
+        verbose_name_plural = _('Pizza Variations')
+        unique_together = ('pizza', 'size')
